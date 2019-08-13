2c8335283ee4f0b7bf510b565e4650e03ec3c00a
Tristan Ganry <tristanganry@gmail.com>
2019-08-12 21:41:44 -0400

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,8 +1,2 @@
 // More info https://github.com/hakimel/reveal.js#configuration
-Reveal.initialize({
-				controls: true,
-				progress: true,
-				center: true,
-				hash: true,
-				transition: 'slide', // none/fade/slide/convex/concave/zoom
-});
+Reveal.initialize();
